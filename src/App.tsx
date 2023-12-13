bd2a8f9272b8dcdb516c2f45ffd55a907a3c0370
Jokikatu oppilas <jokikatu@oppilas.joensuu.fi>
2023-12-13 14:42:01 +0200

    Ylimääräiset sivut poistettu, Oikeat otsikot lisätty

--- src/App.tsx
+++ src/App.tsx
@@ -57,15 +57,11 @@ const App: React.FC = () => (
         <IonTabBar slot="bottom">
           <IonTabButton tab="tab1" href="/tab1">
             <IonIcon aria-hidden="true" icon={triangle} />
-            <IonLabel>Tab 1</IonLabel>
+            <IonLabel>Koti</IonLabel>
           </IonTabButton>
           <IonTabButton tab="tab2" href="/tab2">
             <IonIcon aria-hidden="true" icon={ellipse} />
-            <IonLabel>Tab 2</IonLabel>
-          </IonTabButton>
-          <IonTabButton tab="tab3" href="/tab3">
-            <IonIcon aria-hidden="true" icon={square} />
-            <IonLabel>Tab 3</IonLabel>
+            <IonLabel>Yhteystiedot</IonLabel>
           </IonTabButton>
         </IonTabBar>
       </IonTabs>
--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -7,16 +7,16 @@ const Tab2: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 2</IonTitle>
+          <IonTitle>Yhteystiedot</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 2</IonTitle>
+            <IonTitle size="large">Yhteystiedot</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 2 page" />
+        <ExploreContainer name="Yhteystiedot" />
       </IonContent>
     </IonPage>
   );
